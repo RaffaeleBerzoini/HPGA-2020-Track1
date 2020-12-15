5cf8adab5cf09b4df9ff3771b81b9b361bc49f7a
Raffaele Berzoini <70108790+RaffaeleBerzoini@users.noreply.github.com>
2020-12-15 19:49:13 +0100

    Update IntTable.java

--- IntTable.java
+++ IntTable.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class IntTable {
 
